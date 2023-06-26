09e6ef3434014bc5d6e3a8b580c5ae8738607205
nemixu <s.r.seagrave@gmail.com>
2023-06-26 21:32:48 +0100

    use scrolltosection and remove href being appended to the addressbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,18 @@ import logo from '../Images/blue-orange-logo.png';
 import MobileNavbar from './MobileNavbar';
 
 function Navbar() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleNavClick = (event, id) => {
+    event.preventDefault();
+    scrollToSection(id);
+  };
+
   return (
     <nav className="nav-main justify-content-around fixed-top w-100">
       <div className="nav-main-logo">
@@ -14,22 +26,38 @@ function Navbar() {
         <div className="nav-main-list">
           <ul className="nav-main-list-container">
             <li className="nav-main-list-items">
-              <a aria-label="home" href="#home">
+              <a
+                aria-label="home"
+                href="#home"
+                onClick={(event) => handleNavClick(event, 'home')}
+              >
                 Home
               </a>
             </li>
             <li className="nav-main-list-items">
-              <a aria-label="about" href="#about">
+              <a
+                aria-label="about"
+                href="#about"
+                onClick={(event) => handleNavClick(event, 'about')}
+              >
                 About
               </a>
             </li>
             <li className="nav-main-list-items">
-              <a aria-label="services" href="#services">
+              <a
+                aria-label="services"
+                href="#services"
+                onClick={(event) => handleNavClick(event, 'services')}
+              >
                 Services
               </a>
             </li>
             <li className="nav-main-list-items">
-              <a aria-label="contact" href="#contact">
+              <a
+                aria-label="contact"
+                href="#contact"
+                onClick={(event) => handleNavClick(event, 'contact')}
+              >
                 Contact
               </a>
             </li>
